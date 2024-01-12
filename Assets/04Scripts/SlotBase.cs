c66ad226dedc183f4188c09c56fc53d194a23631
jun0433 <93536029+jun0433@users.noreply.github.com>
2024-01-12 21:54:00 +0900

    01.12 작업
    NPC01 팝업창 UI 작업
    상점 Slot 작업
    상점 스크립트 작성중

---
+++ Assets/04Scripts/SlotBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+interface ISlot
+{
+    public void OnClick_Sell();
+    public void OnClick_Buy();
+    
+}
+public class SlotBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
